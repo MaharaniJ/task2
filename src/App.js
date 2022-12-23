fa335027f36c66a41e7edaa7f41cd60ebd0e544a
MaharaniJ <maharani.jeyaraman@outlook.com>
2022-12-23 19:20:54 +0530

    add changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
 import './App.css';
+import Pricecard from './component/Pricecart';
+function App(){
+  let data = [
+    {
+    plan : "FREE",
+    price : "0",
+    users : "Single User",
+    usersEnabler : true,
+    storage : "5GB Storage",
+    storageEnabler : true,
+   publicproject : "Unlimited Public Projects",
+    publicprojectEnabler : true,
+    communityaccess : "Community Access",
+    communityaccessEnabler : true,
+    privateproject : "Unlimited Private Project",
+    privateprojectEnabler : false,
+    phonesupport : "Dedecated Phone Support",
+    phonesupportEnabler : false,
+    subdomain : "Free Subdomain",
+    subdomainEnabler : false,
+    statusreport : "Monthly Status Report",
+    statusreportEnabler : false 
+  },
+   {  plan : "PLUS",
+   price : "9",
+   users : "5 User",
+   usersEnabler : true,
+   storage : "50GB Storage",
+   storageEnabler : true,
+  publicproject : "Unlimited Public Projects",
+   publicprojectEnabler : true,
+   communityaccess : "Community Access",
+   communityaccessEnabler : true,
+   privateproject : "Unlimited Private Project",
+   privateprojectEnabler : true,
+   phonesupport : "Dedecated Phone Support",
+   phonesupportEnabler : true,
+   subdomain : "Free Subdomain",
+   subdomainEnabler : true,
+   statusreport : "Monthly Status Report",
+   statusreportEnabler : false 
+   },
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    {
+      plan : "PRO",
+      price : "49",
+      users : "Unlimited User",
+      usersEnabler : true,
+      storage : "150GB Storage",
+      storageEnabler : true,
+     publicproject : "Unlimited Public Projects",
+      publicprojectEnabler : true,
+      communityaccess : "Community Access",
+      communityaccessEnabler : true,
+      privateproject : "Unlimited Private Project",
+      privateprojectEnabler : true,
+      phonesupport : "Dedecated Phone Support",
+      phonesupportEnabler : true,
+      subdomain : "Free Subdomain",
+      subdomainEnabler : true,
+      statusreport : "Monthly Status Report",
+      statusreportEnabler : true 
+    
+  }]
+
+
+return <div>
+  <section className='Pricecart py-5'>
+    <div className='container'>
+      <div className='row'>
+        { data.map((e,index) =>{
+           return     <Pricecard data={e} key={index}/>
+        })
+      }
+        
+      </div>
     </div>
-  );
-}
+  </section>
+</div>
 
+}
 export default App;
